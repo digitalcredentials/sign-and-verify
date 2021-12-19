e92eb4cecb1a74f31cf170603b1c5cc552cd6be2
kezike <kezike13@gmail.com>
2021-12-19 18:19:56 -0500

    updated setup script and credential request handler to generate did and include it in credential template

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,14 +3,17 @@
 # Setup issuer configuration
 
 # Install important node modules for this script
-npm install -g fs
-npm install -g minimist
+# npm install -g fs
+# npm install -g minimist
+# npm install -g digitalcredentials/did-cli#main
 
 # Define all file locations that are relevant for setup
 jsonCli=$PWD/bin/json.js
 configFile=$PWD/src/issuer-config.json
+didTmpFile=$PWD/.did.tmp
 
-# Define all config keys
+# Define issuer config keys
+issuerDidKey=ISSUER_DID
 issuerNameKey=ISSUER_NAME
 issuerUrlKey=ISSUER_URL
 issuerStreetKey=ISSUER_STREET
@@ -18,23 +21,40 @@ issuerCityKey=ISSUER_CITY
 issuerStateKey=ISSUER_STATE
 issuerZipKey=ISSUER_ZIP
 
+# Define did-cli keys
+didKey=id
+didSeedKey=secretKeySeed
+
 # Setup config file
 echo {} > $configFile
 
+# Create did
+did id create > $didTmpFile
+
 echo Beginning issuer configuration...
 
-printf "Please enter the official name of your learning institution [ENTER]:\n---> "
-read issuerName
-printf "Please enter the official website of your learning institution [ENTER]:\n---> "
-read issuerUrl
-printf "Please enter the street address of your learning institution [ENTER]:\n---> "
-read issuerStreet
-printf "Please enter the city of your learning institution  [ENTER]:\n---> "
-read issuerCity
-printf "Please enter the state of your learning institution [ENTER]:\n---> "
-read issuerState
-printf "Please enter the postal code of your learning institution [ENTER]:\n---> "
-read issuerZip
+read -p "Please enter the official name of your learning institution [ENTER]: " issuerName
+read -p "Please enter the official website of your learning institution [ENTER]: " issuerUrl
+read -p "Please enter the street address of your learning institution [ENTER]: " issuerStreet
+read -p "Please enter the city of your learning institution [ENTER]: " issuerCity
+read -p "Please enter the state of your learning institution [ENTER]: " issuerState
+read -p "Please enter the postal code of your learning institution [ENTER]: " issuerZip
+read -p "Does your learning institution already own a secret DID seed you would like to use to generate your DID document (yes / no (default)) [ENTER]: " didSeedExists
+didSeedExists=${didSeedExists:-no}
+
+issuerDidSeed=""
+if [ $didSeedExists == "yes" ]
+then
+  printf "Please enter the DID seed for your learning institution [ENTER]: "
+  read issuerDidSeed
+  SECRET_KEY_SEED=$issuerDidSeed did id create > $didTmpFile
+else
+  did id create > $didTmpFile
+  issuerDidSeed=`node $jsonCli --read --key=$didSeedKey --json=$didTmpFile`
+fi
+issuerDid=`node $jsonCli --read --key=$didKey --json=$didTmpFile`
+
+node $jsonCli --write --key=$issuerDidKey --value="$issuerDid" --json=$configFile
 node $jsonCli --write --key=$issuerNameKey --value="$issuerName" --json=$configFile
 node $jsonCli --write --key=$issuerUrlKey --value="$issuerUrl" --json=$configFile
 node $jsonCli --write --key=$issuerStreetKey --value="$issuerStreet" --json=$configFile
@@ -42,7 +62,27 @@ node $jsonCli --write --key=$issuerCityKey --value="$issuerCity" --json=$configF
 node $jsonCli --write --key=$issuerStateKey --value="$issuerState" --json=$configFile
 node $jsonCli --write --key=$issuerZipKey --value="$issuerZip" --json=$configFile
 
-echo Finished issuer configuration! You may rerun this script if you have entered a value in error or if a value is updated in the future. Finally, make sure that the newly generated issuer-config.json file located under the src directory is uploaded to the root of this project in the deployment environment where you will be hosting this service.
+echo
+echo Finished issuer configuration!
+echo
+
+echo [IMPORTANT] Please follow these important instructions:
+echo 1. Please save your DID seed \(see below\) under the environment variable "DID_SEED" in the deployment environment where you will be hosting this service. [IMPORTANT] DO NOT CHECK THIS VALUE INTO A PUBLICLY ACCESSIBLE SOURCE CONTROL MANAGEMENT SYSTEM. THIS IS A SENSITIVE ORGANIZATIONAL SECRET!
+echo 2. Please upload the issuer config \(see below\) under the name "$(basename $configFile)" to the "src" directory of the project located in the deployment environment where you will be hosting this service. You do not need to upload this file to a publicly accessible source control management system. These are organization-specific values.
+echo 3. You may rerun this script or modify the hosted "$(basename $configFile)" if you have entered a value in error or if a value is updated in the future.
+echo
 
 echo Issuer config:
 cat $configFile
+echo
+echo
+
+echo DID info:
+echo DID: $issuerDid
+echo DID seed: $issuerDidSeed
+echo
+
+echo Cleaning up unnecessary data...
+git checkout $configFile
+rm $didTmpFile
+echo Done!
