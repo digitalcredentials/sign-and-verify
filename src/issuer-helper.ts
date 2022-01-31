5ba0d87b74031b64e04882594e7492d8420f0007
kezike <kezike13@gmail.com>
2022-01-31 13:49:11 -0500

    extracted id token from /request/credential endpoint; decoded id token and extracted email from it in credentialRequestHandler; simplified credentialRequestHandler

--- src/issuer-helper.ts
+++ src/issuer-helper.ts
@@ -4,47 +4,46 @@
 
 import fs from 'fs';
 import path from 'path';
-import { get, keys, reduce } from 'lodash';
+import jwt_decode from "jwt-decode";
 import Handlebars from 'handlebars';
 import { dbCredClient } from './database';
 import { default as issuerConfig } from './issuer-config.json';
 
-// NOTE: THIS FUNCTION WAS DESIGNED TO BE AS GENERAL AS POSSIBLE, BUT FEEL FREE TO
-// ALTER IT TO CONTAIN LOGIC FOR RETRIEVING CREDENTIALS FOR LEARNERS IN YOUR ORG
+// NOTE: FEEL FREE TO ALTER IT TO CONTAIN LOGIC FOR RETRIEVING CREDENTIALS FOR LEARNERS IN YOUR ORG
 // NOTE: HOLDER ID IS GENERATED FROM AN EXTERNAL WALLET, NOT THE ISSUER
 // Method for issuer to retrieve credential on behalf of learner
-const credentialRequestHandler = async (holderId: string, credentialId: string): Promise<any> => {
-  // TODO: need to find a way to receive credential type from request payload
-  // Select desired credential schema
+const credentialRequestHandler = async (holderId: string, idToken: string): Promise<any> => {
+  // NOTE: using one credential type for now
+  // Select credential type
   const credentialType = 'Certificate';
-  const schemaFileName = path.resolve(__dirname, `./schema/${credentialType}.json`);
-  const schemaFileString = fs.readFileSync(schemaFileName, { encoding:'utf8' });
-  const schemaObj = JSON.parse(schemaFileString);
-  const primaryKey = schemaObj.id;
-  const schema = schemaObj.schema;
+  // NOTE: CREDENTIAL ID IS THE LEARNER EMAIL IN EARLY DEPLOYMENTS OF THIS CODE
+  // Select credential primary key
+  const primaryKey = 'credentialSubject.email';
+  // Extract email from ID token
+  const idObject: any = jwt_decode(idToken);
+  const email = idObject.email;
+  if (!email) {
+    throw new Error('ID token does not contain email');
+  }
 
   const CredentialModel = await dbCredClient.open();
-  const credentialQuery = { [primaryKey]: credentialId };
+  const credentialQuery = { [primaryKey]: email };
   const credentialRecord = await CredentialModel.findOne(credentialQuery);
   await dbCredClient.close();
   if (!credentialRecord) {
     return Promise.resolve({});
   }
 
-  // Populate remainder of credential config
-  const templateTokens = keys(schema);
-  const learnerCredentialConfig = reduce(
-    templateTokens,
-    (config, token) => {
-      const value = get(credentialRecord, schema[token]);
-      config[token] = value;
-      return config;
-    },
-    {}
-  );
+  // Populate credential config
   const credentialConfig = {
     ...issuerConfig,
-    ...learnerCredentialConfig,
+    CREDENTIAL_NAME: credentialRecord.name,
+    CREDENTIAL_DESC: credentialRecord.description,
+    ISSUANCE_DATE: credentialRecord.issuanceDate,
+    ISSUER_NAME: credentialRecord.issuer.name,
+    ISSUER_URL: credentialRecord.issuer.url,
+    ISSUER_IMAGE: credentialRecord.issuer.image,
+    LEARNER_NAME: credentialRecord.credentialSubject.name,
     LEARNER_DID: holderId
   };
 
