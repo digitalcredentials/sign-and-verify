334a7e2f9ac213c75c9e99db0069bca680200833
kezike <kezike13@gmail.com>
2022-03-21 19:22:25 -0400

    added environment variables for db name and lms table name

--- src/database.ts
+++ src/database.ts
@@ -6,6 +6,8 @@ const { MongoClient } = require('mongodb');
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME as string;
+const DB_TABLE = process.env.DB_TABLE as string;
 const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}`;
 
 class DatabaseClient {
@@ -51,4 +53,4 @@ class DatabaseClient {
   }
 }
 
-export const dbCredClient = new DatabaseClient(DB_URI, 'sign-and-verify', 'credential-recipients');
+export const dbCredClient = new DatabaseClient(DB_URI, DB_NAME, DB_TABLE);
