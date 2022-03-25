80733fc49b4fcfc07c74171b3ee6133ac726dc21
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-03-24 21:27:03 -0400

    Add imports to package.json, move /request/credential route towards top.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,6 @@
 declare module '@digitalcredentials/bnid';
+declare module '@digitalcredentials/did-method-key';
+declare module '@digitalcredentials/ed25519-verification-key-2020';
+declare module '@digitalcredentials/x25519-key-agreement-key-2020';
+declare module 'crypto-ld';
+declare module 'lru-cache';
