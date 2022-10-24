698d39af5b55a93980e8275e1f7a65b0aac08643
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-10-24 14:09:06 -0400

    Update certificate.

--- src/templates/Certificate.ts
+++ src/templates/Certificate.ts
@@ -1,35 +1,46 @@
 export function composeCredential (issuerDid: string, holderDid: string, credentialRecord: any): any {
   const credential: any = {
     '@context': [
-      'https://www.w3.org/2018/credentials/v1',
-      'https://w3id.org/security/suites/ed25519-2020/v1',
-      'https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/jff-vc-edu-plugfest-1-context.json'
+      "https://www.w3.org/2018/credentials/v1",
+      "https://purl.imsglobal.org/spec/ob/v3p0/context.json"
     ],
-    type: ['VerifiableCredential', 'OpenBadgeCredential'],
+    id: "urn:uuid:a63a60be-f4af-491c-87fc-2c8fd3007a58",
+    type: [
+      "VerifiableCredential",
+      "OpenBadgeCredential"
+    ],
+    name: "JFF x vc-edu PlugFest 2 Interoperability",
     issuer: {
-      type: 'Profile',
+      type: [
+        "Profile",
+      ],
       id: issuerDid,
-      name: credentialRecord.issuer.name,
-      // url: credentialRecord.issuer.url,
-      // image: credentialRecord.issuer.image
+      name: credentialRecord.issuer.name
+      url: "https://www.jff.org/",
+      image: "https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png"
     },
     issuanceDate: credentialRecord.issuanceDate,
     credentialSubject: {
-      type: 'AchievementSubject',
+      type: ["AchievementSubject"],
       id: holderDid,
-      // name: credentialRecord.credentialSubject.name,
       achievement: {
-        type: 'Achievement',
-        name: credentialRecord.name,
-        description: credentialRecord.description,
+        id: "urn:uuid:bd6d9316-f7ae-4073-a1e5-2f7f5bd22922",
+        type: [
+          "Achievement"
+        ],
+        name: "JFF x vc-edu PlugFest 2 Interoperability",
+        description: "This credential solution supports the use of OBv3 and w3c Verifiable Credentials and is interoperable with at least two other solutions.  This was demonstrated successfully during JFF x vc-edu PlugFest 2.",
         criteria: {
-          type: 'Criteria',
-          narrative: 'The first cohort of the JFF Plugfest 1 in May/June of 2022 collaborated to push interoperability of VCs in education forward.'
+          narrative: "Solutions providers earned this badge by demonstrating interoperability between multiple providers based on the OBv3 candidate final standard, with some additional required fields. Credential issuers earning this badge successfully issued a credential into at least two wallets.  Wallet implementers earning this badge successfully displayed credentials issued by at least two different credential issuers."
         },
-        image: 'https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/images/plugfest-1-badge-image.png'
+        image: {
+          id: "https://w3c-ccg.github.io/vc-ed/plugfest-2-2022/images/JFF-VC-EDU-PLUGFEST2-badge-image.png",
+          type: "Image"
+        }
       }
     }
   }
+
   if (credentialRecord.expirationDate) {
     credential.expirationDate = credentialRecord.expirationDate;
   }
